e2fba0f486e14f6e4c272e31cebf1e9b1912f7aa
lakshmanaram <lakshmanaram.n@gmail.com>
2016-07-07 05:58:41 +0530

    adds code to flush keystrokes belonging to previous execution

--- incor/EventHandler.py
+++ incor/EventHandler.py
@@ -1,53 +1,48 @@
 import os
 import signal
-import time
 from watchdog.events import FileSystemEventHandler
-from subprocess32 import Popen,PIPE,call
+from subprocess32 import call
 import psutil
+
 
 class EventHandler(FileSystemEventHandler):
 
     # TODO Add docstrings
 
-    parentpid = None
+    parentPid = None
     cmd = None
-    newcmd = False
+    newCmd = False
     lastCall = ''
-    existing_Children = None
 
     def __init__(self, path):
         self.path = path
 
     def on_modified(self, event):
-        # Checks whether the created event is not a directory event
         if not event.is_directory:
+            # Checks whether the created event is not a directory event
             cur_path = event.src_path
             file_extension = cur_path.split('.')[-1]
             if file_extension == 'py':
+                parent = ''
                 try:
-                    parent = psutil.Process(self.parentpid)
+                    parent = psutil.Process(self.parentPid)
                 except psutil.NoSuchProcess:
                     print 'No such process'
-                Children = parent.children(recursive=True)
-                print Children
-                print self.existing_Children
-                for process in Children:
-                    if process not in self.existing_Children:
-                        process.send_signal(signal.SIGTERM)
-                # call('clear', shell=True)
-                print self.lastCall
-                self.lastCall = 'python ' + cur_path + ' call terminated'
+                children = parent.children(recursive=True)
+                for process in children:
+                    process.send_signal(signal.SIGKILL)
+                call('clear', shell=True)
+                if children:
+                    print 'Previously executing processes terminated'
                 print('issuing system call - python ' + cur_path)
                 self.cmd = 'python ' + cur_path
-                self.newcmd = True
-                time.sleep(1)
+                self.newCmd = True
 
     def on_created(self, event):
-        cur_path = ''
         if not event.is_directory:
             cur_path = event.src_path
             file_extension = cur_path.split('.')[-1]
-            if file_extension in ['py','cpp','c']:
+            if file_extension in ['py', 'cpp', 'c']:
                 name = 'template.' + file_extension
                 f_created = open(cur_path, 'rw')
                 if f_created.read() == '':
--- incor/incor.py
+++ incor/incor.py
@@ -2,35 +2,32 @@ import time
 import sys
 import os
 from watchdog.observers import Observer
+from termios import tcflush, TCIOFLUSH
 import incor
 from EventHandler import EventHandler
-import psutil
 from subprocess import call
 
-def main():
 
+def main():
     path = sys.argv[1] if len(sys.argv) > 1 else '.'
     if path == '--version':
         print('incor v' + incor.__version__)
         return
     observer = Observer()
-    eventHandler = EventHandler(path)
-    eventHandler.parentpid = os.getpid()
-    observer.schedule(eventHandler, path, recursive=True)
+    eventhandler = EventHandler(path)
+    eventhandler.parentPid = os.getpid()  # parent process pid
+    observer.schedule(eventhandler, path, recursive=True)
     observer.start()
-    parent = None
-    try:
-        parent = psutil.Process(eventHandler.parentpid)
-    except psutil.NoSuchProcess:
-        print 'No such process'
-    eventHandler.existing_Children = parent.children(recursive=True)
-    print eventHandler.existing_Children
     try:
         while True:
-            if eventHandler.newcmd:
-                eventHandler.newcmd = False
-                call(eventHandler.cmd,shell=True)
-
+            if eventhandler.newCmd:
+                eventhandler.newCmd = False
+                sys.stdout.flush()
+                sys.stdout.flush()
+                tcflush(sys.stdin, TCIOFLUSH)
+                # flushes the keystrokes of the previously terminated program
+                call(eventhandler.cmd, shell=True)
+            time.sleep(1)
     except KeyboardInterrupt:
         observer.stop()
     observer.join()
