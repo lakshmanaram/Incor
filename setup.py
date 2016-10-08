b7cca744ed137d84fbe98b4b94b2f2a1ec904119
Aniket Maithani <maithani.aniket@gmail.com>
2016-10-09 02:50:41 +0530

    chore(InCOR): PEP8 formatting - typo in README corrected

--- setup.py
+++ setup.py
@@ -8,8 +8,9 @@ except ImportError:
 setup(name='incor',
       version=incor.__version__,
       install_requires=['watchdog>=0.8.2', 'psutil>=4.3.0'],
-      description='INstant COde Runner compiles and executes the programs present in the mentioned folder '
-                  'instantaneously as and when changes are .',
+      description='INstant COde Runner compiles and executes the programs' +
+      'present in the mentioned folder' +
+      'instantaneously as and when changes are.',
       url='https://github.com/lakshmanaram/Incor',
       author='lakshmanaram, srivatsan-ramesh',
       author_email='lakshmanaram.n@gmail.com, sriramesh4@gmail.com',
@@ -18,5 +19,6 @@ setup(name='incor',
       entry_points={
           'console_scripts': ['incor=incor.main:main'],
       },
-      keywords=['Algorithms', 'Competitive Programming', 'Automatic', 'run', 'compile', 'execute'],
+      keywords=['Algorithms', 'Competitive Programming',
+                'Automatic', 'run', 'compile', 'execute'],
       zip_safe=False)
